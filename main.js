9ed1c234079d108c15b308955d7626c27607f818
Ahmet Burak Budak <ahmetburak.budak@gmail.com>
2020-02-08 17:05:58 +0300

    add menus

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
+    console.log(process.platform);
+
     console.log("it works");
 
     mainWindow = new BrowserWindow({});
@@ -17,5 +19,52 @@ app.on('ready', () => {
             protocol: "file",
             slashes: true
         })
-    )
-})
+    );
+
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [{
+    label: "File",
+    submenu: [
+        {
+            label: "Add New Todo"
+        },
+        {
+            label: "Delete All"
+        },
+        {
+            label: "Exit",
+            accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+            role: "quit"
+        }
+    ]
+}
+]
+
+if (process.platform == "darwin") {
+    mainMenuTemplate.unshift({
+        label: app.getName(),
+        role: "TODO"
+    })
+}
+
+if (process.env.NODE_ENV !== "production") {
+    mainMenuTemplate.push({
+        label: "Dev Tools",
+        submenu: [
+            {
+                label: "Open DevTools",
+                click(item, focusedwindow) {
+                    focusedwindow.toggleDevTools();
+                }
+            },
+            {
+                label: "Refresh",
+                role: "reload"
+            }
+        ]
+    })
+}
