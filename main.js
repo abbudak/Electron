32e6928babf30828dd87253bd2e1bad12bc539c6
Ahmet Burak Budak <ahmetburak.budak@gmail.com>
2020-02-09 13:45:53 +0300

    commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 
@@ -21,10 +21,18 @@ app.on('ready', () => {
         })
     );
 
-
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
+
 });
+
+// ipcMain.on("key", (err, data) =>{
+//     console.log(data);
+// })
+
+ipcMain.on('key', function (event, arg) {
+    console.log(data);
+})
 
 const mainMenuTemplate = [{
     label: "File",
